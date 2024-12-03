5ba9e8312713499cda496a976fc6355e19fb50a6
Sriram V <152197781+SriramV1212@users.noreply.github.com>
2024-12-02 21:52:00 -0500

    Time: 1 ms (34.61%), Space: 17.3 MB (6.41%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,49 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        a=["(","{","["]
+        b=[")","}","]"]
+        p=0
+        q=0
+        r=0
+        l=[]
+        count=0
+        if (len(s)%2)!=0 or s[0] in b:
+            return(False)
+        for i in range(len(s)):
+            count+=1
+            if s[i]==a[0]:
+                p+=1
+                l.append(s[i])
+            if s[i]==a[1]:
+                q+=1
+                l.append(s[i])
+            if s[i]==a[2]:
+                r+=1
+                l.append(s[i])
+            if s[i]==b[0] and p==0:
+                break
+            if s[i]==b[1] and q==0:
+                break
+            if s[i]==b[2] and r==0:
+                break
+            if s[i]==b[0] and p>0:
+                if s[i-1]==a[0] or (q==0 and r==0) or l[len(l)-1]==a[0]:
+                    p-=1
+                    l.pop()
+            if s[i]==b[1] and q>0:
+                if s[i-1]==a[1] or (p==0 and r==0) or l[len(l)-1]==a[1]:
+                    q-=1
+                    l.pop()
+            if s[i]==b[2] and r>0:
+                if s[i-1]==a[2] or (p==0 and q==0) or l[len(l)-1]==a[2]:
+                    r-=1
+                    l.pop()
+        if p==0 and q==0 and r==0 and count==len(s):
+            return(True)
+        else:
+            return(False)
+                
+        
+
+
+        
