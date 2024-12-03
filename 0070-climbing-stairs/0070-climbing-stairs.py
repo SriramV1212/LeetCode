a143bfbb735ea4c126d9f519e258fd40b9d2362f
Sriram V <152197781+SriramV1212@users.noreply.github.com>
2024-12-03 00:45:31 -0500

    Time: 0 ms (100%), Space: 17.4 MB (5.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,7 +2,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         p=1
         c=1
-        for i in range(1,n):
+        for i in range(n-1):
             t=c
             c=p+c
             p=t
