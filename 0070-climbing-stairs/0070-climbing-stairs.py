983307c3c8b7a7aaf49a7de4508fa8c3771e577e
Sriram V <152197781+SriramV1212@users.noreply.github.com>
2024-12-03 18:59:28 -0500

    Time: 3 ms (100%), Space: 17.2 MB (6.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        p=1
-        c=1
-        for i in range(n-1):
-            t=c
-            c=p+c
-            p=t
-        return c
+        def fact(m):
+          c=1
+          for i in range(1,m+1):
+              c*=i
+          return(c)
+        ways=0
+        for i in range(n+1):
+             for j in range(n+1):
+                 if (2*i + 1*j)==n:
+                      k=fact(i+j)//(fact(i) * fact(j))
+                      ways+=k
+        return(ways)
+
+        
+
 
         
