403a4e265610c0fc28e482b3acedcb07e42b97dc
Sriram V <152197781+SriramV1212@users.noreply.github.com>
2024-12-22 12:53:50 -0500

    Time: 0 ms (100%), Space: 17.9 MB (5.27%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,20 +1,27 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        n = len(needle)
-        h = len(haystack)
-        List = []
-        for i in range(h):
-            z = haystack[i:i+n]
-            if needle == z:
-                List.append(i)
-
-        if len(List)!=0:
-            return(List[0])
-        else:
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        l=[]
+        s=""
+        c=""
+        j=0
+        for i in range(len(haystack)):
+            if haystack[i]==needle[0]:
+                l.append(i)
+        
+        for x in l:
+            c=haystack[x:]
+            for i in range(len(needle)):
+                if i<len(c) and needle[j]==c[i] and ((s=="")or needle.startswith(s)):
+                  s+=c[i]
+                  j+=1
+                else:
+                  s=""
+                  j=0
+                  break
+            if s==needle:
+              break
+        if s!=needle:
             return(-1)
+        else:
+            return(x)
         
